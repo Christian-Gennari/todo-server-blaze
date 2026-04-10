d2eb68b0166bf33d5bf4bd48258d4a5d53186a7d
discotomten <linkaan_lonn@hotmail.com>
2026-04-10 13:08:11 +0200

    css fixed

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 using TodoServerBlaze.Components;
-
+using TodoServerBlaze;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
--- TodoState.cs
+++ TodoState.cs
@@ -1,34 +1,25 @@
 namespace TodoServerBlaze
 {
     public class TodoState
-{
-    public List<TodoDTO> Todos { get; set; } = [];
-
-    public event Action? OnChange;
-
-    private void NotifyStateChanged() => OnChange?.Invoke();
-
-    public void AddTodo(TodoDTO todo)
     {
-        Todos.Add(todo);
-        NotifyStateChanged();
-    }
+        public List<TodoDTO> Todos { get; set; } = [];
 
-    public void AddCheck(TodoDTO todo)
-    {
-        todo.IsChecked = true;
-        NotifyStateChanged();
-    }
-    public void CreateNew()
-    {
-        var todo = new TodoDTO()
+        public event Action? OnChange;
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+
+        public void AddTodo(TodoDTO todo)
         {
-            Content = "",
-            IsChecked = false,
-        };
+            Todos.Add(todo);
+            NotifyStateChanged();
+        }
 
-        Todos.Add(todo);
-        NotifyStateChanged();
+        public void AddCheck(TodoDTO todo)
+        {
+            todo.IsChecked = true;
+            NotifyStateChanged();
+        }
+
     }
 
 }
