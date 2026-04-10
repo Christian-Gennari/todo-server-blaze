8e6116b549b5ef7632622e63353c505da984c076
Christian Gennari <christiangennari61@gmail.com>
2026-04-10 11:00:55 +0000

    fix thrice

--- TodoDto.cs
+++ TodoDto.cs
@@ -1,9 +1,9 @@
 namespace TodoServerBlaze
 {
 
-    public class TodoDTO
+    public class TodoDTO (string? Content, bool IsChecked)
     {
-        public string? Content { get; set; } = "";
-        public bool IsChecked { get; set; }
+        public string? Content { get; set; } = Content;
+        public bool IsChecked { get; set; } = IsChecked;
     }
 }
